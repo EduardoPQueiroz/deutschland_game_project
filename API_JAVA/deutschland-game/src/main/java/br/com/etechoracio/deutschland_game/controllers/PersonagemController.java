c068d3b6f20ffeeaaca2c18b8543be2c9da58203
Matheus Pinter <matheuspinter08@gmail.com>
2024-09-23 12:12:55 -0300

    feat: Improved function getPersonagemById

--- API_JAVA/deutschland-game/src/main/java/br/com/etechoracio/deutschland_game/controllers/PersonagemController.java
+++ API_JAVA/deutschland-game/src/main/java/br/com/etechoracio/deutschland_game/controllers/PersonagemController.java
@@ -4,6 +4,8 @@ import br.com.etechoracio.deutschland_game.entities.Personagens;
 import br.com.etechoracio.deutschland_game.exceptions.PersonagemIdNotFoundException;
 import br.com.etechoracio.deutschland_game.services.PersonagemSevice;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +19,7 @@ public class PersonagemController {
     private PersonagemSevice personagemService;
 
     @GetMapping("/{id}")
-    public Personagens getPersonagemById(@PathVariable("id") long id) throws PersonagemIdNotFoundException {
-        return personagemService.getPersonagensById(id);
+    public ResponseEntity<Personagens> getPersonagemById(@PathVariable("id") long id) throws PersonagemIdNotFoundException {
+        return ResponseEntity.status(HttpStatus.OK).body(personagemService.getPersonagensById(id));
     }
 }
