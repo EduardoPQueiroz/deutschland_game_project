1055bca022aaf67766d4c26fb1a026a9ab59104b
PedroLudovic144 <pedroludovic5@gmail.com>
2024-09-24 16:36:29 -0300

    Fix:Fix the Dialogos(controller,service and repository)

--- API_JAVA/deutschland-game/src/main/java/br/com/etechoracio/deutschland_game/controllers/DialogosController.java
+++ API_JAVA/deutschland-game/src/main/java/br/com/etechoracio/deutschland_game/controllers/DialogosController.java
@@ -3,18 +3,28 @@ package br.com.etechoracio.deutschland_game.controllers;
 import br.com.etechoracio.deutschland_game.entities.Dialogos;
 import br.com.etechoracio.deutschland_game.services.DialogosService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/dialogos")
 public class DialogosController {
     @Autowired
     private DialogosService service;
-    @GetMapping("/listar")
-    public List<Dialogos> listar(){return service.listar();}
 
-//    @PostMapping
-//    public Dialogos cadastrar(@RequestBody Dialogos dialogos){return service.cadastrar(dialogos);}
+    @GetMapping("/listar")
+    public ResponseEntity<Dialogos> listarDialogoAleatorio() {
+        List<Dialogos> dialogos = service.listar();
+        if(dialogos.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        Random random = new Random();
+        int randomIndex = random.nextInt(dialogos.size());
+        Dialogos dialogoSorterteado = dialogos.get(randomIndex);
+        return new ResponseEntity<>(dialogoSorterteado, HttpStatus.OK);
+    }
 }
