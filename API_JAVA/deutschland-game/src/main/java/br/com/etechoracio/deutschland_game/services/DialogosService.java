41b8b660476d4fa2bc18c878d43b51fcc0e11dd5
PedroLudovic144 <pedroludovic5@gmail.com>
2024-09-27 18:44:46 -0300

    fix: Change between label controller and service, for have  less logic in contoller label and do  the function obterDialogosAleatorios return 8 dialogues

--- API_JAVA/deutschland-game/src/main/java/br/com/etechoracio/deutschland_game/services/DialogosService.java
+++ API_JAVA/deutschland-game/src/main/java/br/com/etechoracio/deutschland_game/services/DialogosService.java
@@ -5,11 +5,35 @@ import br.com.etechoracio.deutschland_game.repositories.DialogosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class DialogosService {
+
     @Autowired
     private DialogosRepository repository;
-    public List<Dialogos> listar(){return repository.findAll();}
+
+    public List<Dialogos> listar() {
+        return repository.findAll();
+    }
+
+    public List<Dialogos> obterDialogosAleatorios() {
+        List<Dialogos> dialogos = listar();
+        if (dialogos.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        Random random = new Random();
+        List<Dialogos> dialogosSorteados = new ArrayList<>();
+
+
+        for (int i = 0; i < 8 && !dialogos.isEmpty(); i++) {
+            int randomIndex = random.nextInt(dialogos.size());//Me fale se essa perte pode melhorar
+            dialogosSorteados.add(dialogos.remove(randomIndex));
+        }
+
+        return dialogosSorteados;
+    }
 }
