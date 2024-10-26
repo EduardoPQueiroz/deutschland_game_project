72fbfe95413c0247d2946ddc5c0a0ffb4b0fa0dc
M4theusj <matheusjacob312@hotmail.com>
2024-10-26 12:59:19 -0300

    feat: Coluna periodo adicionado

--- API_JAVA/deutschland-game/src/main/java/br/com/etechoracio/deutschland_game/dtos/LoadEraDataDto.java
+++ API_JAVA/deutschland-game/src/main/java/br/com/etechoracio/deutschland_game/dtos/LoadEraDataDto.java
@@ -3,5 +3,5 @@ package br.com.etechoracio.deutschland_game.dtos;
 import br.com.etechoracio.deutschland_game.entities.Era;
 import br.com.etechoracio.deutschland_game.entities.EraSprites;
 
-public record LoadEraDataDto(Long id, String nome_era, String sprite) {
+public record LoadEraDataDto(Long id, String nome_era, String periodo, String sprite) {
 }
